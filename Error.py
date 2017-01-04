ac1b887ccfbaac67af55bdf9b2532f67ed6c57c0
Kunal_B <Kunalbaru@gmail.com>
2017-01-04 15:20:09 +0530

    Update Error.py

--- Error.py
+++ Error.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python2.6
-file=raw_input('Enter File Name:')
-try:
+#!/usr/bin/python2.6 #Path for Binaries
+file=raw_input('Enter File Name:') #this is for taking input from user
+try: #try and except is used to verify something
  fhand=open(file)
 except:
  print 'This is not the right file',file
  exit()
 count=0
 for line in fhand:
- if 'Error' in line:
+ if 'Error' in line: # Error is the keyword which we are searching in the text file, we can change the Keyword as per our requirement.
   count=count+1
 print 'Required word comes',count, 'times'
